0182b86bf15610e29e1ff13351ca6a4efc5d0e39
Lee Olayvar <leeolayvar@gmail.com>
2015-02-27 19:23:07 +0000

    Added sass compiling

--- muta.go
+++ muta.go
@@ -8,6 +8,7 @@ import (
 	"github.com/leeola/muta"
 	"github.com/leeola/muta-frontmatter"
 	"github.com/leeola/muta-markdown"
+	"github.com/leeola/muta-sass"
 	"github.com/leeola/muta-template"
 )
 
@@ -33,17 +34,28 @@ func fmTyper(st string) (t interface{}, err error) {
 	return
 }
 
-func main() {
-	muta.Task("markdown", func() (*muta.Stream, error) {
-		s := muta.Src("./*.md").
-			Pipe(frontmatter.FrontMatter(fmTyper)).
-			Pipe(markdown.Markdown()).
-			Pipe(template.Template("./.muta/templates")).
-			Pipe(prettyurls.Prettyurls()).
-			Pipe(muta.Dest("./build"))
-		return s, nil
-	})
+func Markdown() (*muta.Stream, error) {
+	s := muta.Src("./*.md").
+		Pipe(frontmatter.FrontMatter(fmTyper)).
+		Pipe(markdown.Markdown()).
+		Pipe(template.Template("./.muta/templates")).
+		Pipe(prettyurls.Prettyurls()).
+		Pipe(muta.Dest("./build"))
+	return s, nil
+}
 
-	muta.Task("default", "markdown")
+func Sass() (*muta.Stream, error) {
+	s := muta.Src("./.muta/sass/*.scss").
+		Pipe(sass.Sass()).
+		Pipe(muta.Dest("./build/css"))
+	return s, nil
+}
+
+func main() {
+	muta.Task("markdown", Markdown)
+	muta.Task("sass", Sass)
+
+	muta.Task("build", "markdown", "sass")
+	muta.Task("default", "build")
 	muta.Te()
 }
